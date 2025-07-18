5da10dea906f04dc0a26bf2f59ba90681c591cab
rl627 <rl627@drexel.edu>
2025-07-18 09:50:18 -0400

    renamed extension to lanugage agonistic extension #1

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,10 @@ import { PythonDocumentSymbolProvider } from './documentSymbolProvider';
 export function activate(context: vscode.ExtensionContext) {
 
 	// Show message when extension loads
-	vscode.window.showInformationMessage('v1 LOADED');
+	vscode.window.showInformationMessage('Friend code outliner loaded');
 
 
-	console.log('Congratulations, your extension "python-outline-code-sections" is now active!');
+	console.log('Congratulations, your extension "friendly-code-outlines-vscode" is now active!');
 
 	// Register the document symbol provider for Python files
 	context.subscriptions.push(
@@ -24,10 +24,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('python-outline-code-sections.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('friendly-code-outlines-vscode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('v1 command called');
+		vscode.window.showInformationMessage('Hello from your friendly code outliner');
 	});
 
 	context.subscriptions.push(disposable);
