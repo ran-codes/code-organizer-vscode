a14ab0d407f962e11d9f12a0d0151353d4fc1c7e
rl627 <rl627@drexel.edu>
2025-07-21 11:37:28 -0400

    sql test file updated to incldue third leve

--- test-files/test.sql
+++ test-files/test.sql
@@ -22,16 +22,87 @@ USE myapp;
 -- code
 -- code
 
--- 1.1 Tables ----
+---- 1.1 Tables ----
+
+------ 1.1.1 Users ----
+
 CREATE TABLE users (
     id INT PRIMARY KEY,
     name VARCHAR(100)
 );
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+
+
+------ 1.1.1 Orders ----
 
 CREATE TABLE orders (
     id INT PRIMARY KEY,
     user_id INT
 );
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+
+
+---- 1.1 Row Level Security ----
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
+-- code
 -- code
 -- code
 -- code
@@ -76,7 +147,7 @@ SELECT * FROM users;
 -- code
 -- code
 
--- 2.1 Joins ----
+---- 2.1 Joins ----
 SELECT u.name, o.id 
 FROM users u 
 JOIN orders o ON u.id = o.user_id;
